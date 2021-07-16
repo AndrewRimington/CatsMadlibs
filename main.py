2882288cdda552b5ecda5e1a490c0f7424f7873e
Frostbyte-Cat1 <87213468+AndrewRimington@users.noreply.github.com>
2021-07-16 21:18:38 +0800

    Cleaning up of my old code

---
+++ main.py
@@ -0,0 +1,10 @@
+noun1 = input("Character 1: ")
+noun2 = input("Character 2:")
+adjective = input("Adjective: ")
+
+madlib = f"""Once upon a time, there was a {noun1} and {noun2}. The {noun1} grew hungry and decided to eat the 
+{noun2}. {noun2} did not want to get eaten so it {adjective} {noun1}. The {noun1} was sad that {noun2} had 
+{adjective} {noun1} and the {noun1} also felt bad for wanting to eat {noun2}. As a result, both {noun1} and 
+{noun2} decided to go to McDonald's and they lived happily ever after. The end. """
+
+print(madlib)
